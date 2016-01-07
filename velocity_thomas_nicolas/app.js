368eaaa01a53ce4b5023a5db7d613cbc1dd56283
Thomas Bricquet <thomas.bricquet@gmail.com>
2016-01-07 10:05:33 +0100

    last modif

--- velocity_thomas_nicolas/app.js
+++ velocity_thomas_nicolas/app.js
@@ -1,34 +1,32 @@
 	var pingouin = {
 		p1 :function(){
-			$("#bloc1").click(function(){
+			$("#bloc1").on('mouseenter', function(){
 				$(this).velocity
 				({translateX: '1000px'},10000);
 			});
-			$("#bloc2").click(function(){
+			$("#bloc2").on('mouseenter', function(){
 				$(this).velocity
 				({translateX: '1200px', rotateZ: '-1080deg'},10000);
 			});
-			$("#bloc3").click(function(){	
+			$("#bloc3").on('mouseenter', function(){	
 				$(this).velocity
 				({width: "100%"},10000);
 			});
-			$("#bloc4").click(function(){
+			$("#bloc4").on('mouseenter', function(){
 				$(this).velocity
 				({translateX: "1400px", opacity: 0},10000);
 			});
 		},
 		p2 :function(){
-			$("#bloc1").on('click', function(){
+			$("#bloc1").on('mouseenter', function(){
 				$(this).velocity(
 				{translateX: '1000px'}).velocity
 				({translateY: '500px'}).velocity
 				({rotateZ: '-1080deg'}).velocity
-				({translateX: '0', translateY: '0'}).velocity
+				({translateX: '0', translateY: '0', rotateZ: "1080deg"}).velocity
 			});
 		}
 	};
-
-
 $(function(){
 	var page = $('[x-page]').attr('x-page');
 	pingouin[page]();
